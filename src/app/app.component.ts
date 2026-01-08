e2bbb78f4825bc386934b9b502c70f5e53d5a890
giovannagrigo <giovanna1grigo@gmail.com>
2026-01-08 08:31:46 -0300

    Atualizações automáticas na PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UpdateService } from './shared/services/update.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ng-fokus';
+export class AppComponent implements OnInit {
+  constructor(
+    private updateService: UpdateService
+  ) {
+
+  }
+
+  async ngOnInit() {
+    const hasUpdate = await this.updateService.checkForUpdate();
+
+    if(hasUpdate) {
+      console.log('Atualização encontrada durante a inicialização')
+    }
+  }
 }
