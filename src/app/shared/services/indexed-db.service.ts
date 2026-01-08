4ac2d57a2381e5e74e76e551d45e3f1078b408e7
giovannagrigo <giovanna1grigo@gmail.com>
2026-01-08 13:27:01 -0300

    Criptografia de dados

--- src/app/shared/services/indexed-db.service.ts
+++ src/app/shared/services/indexed-db.service.ts
@@ -2,6 +2,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject, filter, Observable, switchMap, take } from 'rxjs';
 import { TaskItem } from '../components/task-manager/task-item';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,8 @@ export class IndexedDBService {
   private readonly db$ = new BehaviorSubject<IDBDatabase | null>(null);
   private readonly store = { name: 'tasks', key: 'uuid' };
   private dbReady$ = new BehaviorSubject<boolean>(false);
+
+  private readonly secretKey = 'T4WC_4FHyQmiOodG1R073bAY3E9OhiEEHO7Cu-u2a8g';
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object
@@ -36,6 +39,15 @@ export class IndexedDBService {
     }
   }
 
+  private encrypt(data: any): string {
+    return CryptoJS.AES.encrypt(JSON.stringify(data), this.secretKey).toString();
+  }
+
+  private decrypt(encryptedData: string): any {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, this.secretKey);
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  }
+
   private waitForDB(): Observable<boolean> {
     return this.dbReady$.pipe(
       filter(ready => ready),
@@ -56,6 +68,13 @@ export class IndexedDBService {
   addTask(task: TaskItem): Observable<TaskItem> {
     return this.waitForDB().pipe(
       switchMap(() => new Observable<TaskItem>(obs => {
+        const encryptedTask = {
+          uuid: task.uuid,
+          encryptedData: this.encrypt({
+            ...task
+          })
+        }
+
         const req = this.store$.add(task);
         req.onsuccess = () => { obs.next(task); obs.complete() };
         req.onerror = () => obs.error('Add task failed');
@@ -67,7 +86,22 @@ export class IndexedDBService {
     return this.waitForDB().pipe(
       switchMap(() => new Observable<TaskItem[]>(obs => {
         const req = this.store$.getAll();
-        req.onsuccess = () => { obs.next(req.result); obs.complete() };
+        req.onsuccess = () => {
+          try {
+            const decryptedTasks = req.result.map(encryptedTask => {
+              const decryptedData = this.decrypt(encryptedTask.encryptedData);
+              return {
+                ...decryptedData,
+                uuid: encryptedTask.uuid
+              };
+            });
+
+            obs.next(decryptedTasks);
+            obs.complete();
+          } catch (error) {
+            obs.error('Decryption failed');
+          }
+        };
         req.onerror = () => obs.error('List tasks failed');
       }))
     )
